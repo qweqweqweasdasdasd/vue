c7a70caa4907e3d9e01a5a56b70fd42a865a506e
lele <15836020238@163.com>
2019-04-03 02:14:47 +0800

    webpack config

--- webpackstudy/src/main.js
+++ webpackstudy/src/main.js
@@ -18,8 +18,26 @@
 //webpack-dev-server 再本地项目中 安装 webpack || npm i webpack -D
 //npm run dev   //自动打印 自动刷新
 
+//页面放在内存里 npm i html-webpack-plugin
+
 import $ from 'jquery';
 
+//导入link css 资源
+import './css/index.css';
+import './css/index.less';
+import './css/index.scss';
+//webpack默认只能打包处理js 文件 无法处理非js 文件
+//如果需要处理非js 文件的话需要一些loader 
+//npm i style-loader css-loader -D
+//在webpack.config.js 这个配置文件在里面 新增一个配置的节点 叫做 module 
+
+//npm i less-loader npm i less -D
+
+//npm i sass-loader -D npm i node-sass -D
+
+//注意 发现不是一个js 文件 在webpack.config.js 文件内查询有没有对应的第三方laoder 
+//如果找到了都对应的规则的时候，使用该loader处理
+//执行的的顺序是从后到钱
 $(function(){
     $('li:odd').css('backgroundColor','red')
     $('li:even').css('backgroundColor',function(){
--- webpackstudy/webpack.config.js
+++ webpackstudy/webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path')
 
+const webpack = require('webpack')
+
+//导入内存中生成html的插件 只要是插件都要放在 plugin 
+const htmlWebpackPlugin = require('html-webpack-plugin')
 
 //node 向外暴露一个配置文件
 module.exports = {
@@ -7,5 +11,26 @@ module.exports = {
     output:{
         path:path.join(__dirname,'./dist'), //指定 打包好的路径 
         filename:'bundle.js'    //这个是指定的输入的文件名称
+    },
+    devServer:{             //配置Devserver 命令的第二种方式
+        open:true,          //自动打开游览器
+        port:3000,           //端口
+        contentBase:'src',
+        hot:true
+    },
+    plugins:[       //配置插件的节点
+        new webpack.HotModuleReplacementPlugin(),            //热更新的对象
+
+        new htmlWebpackPlugin({
+            template:path.join(__dirname ,'./src/index.html'),
+            filename:'index.html'
+        })         //创建一个内存中生成html插件
+    ],
+    module:{    //这个节点 用于配置 所有的第三方模块
+        rules:[
+            {test:/\.css$/,use:['style-loader','css-loader']},   //
+            {test:/\.less$/,use:['style-loader','css-loader','less-loader']},
+            {test:/\.scss$/,use:['style-loader','css-loader','sass-loader']}
+        ]
     }
 }
